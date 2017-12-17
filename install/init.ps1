70e536037306eb03182b7991a3004debcbd570e1
Gulzar Ahmed <gulzarhome54@gmail.com>
2017-12-17 16:55:00 +0530

    tweaks

--- install/init.ps1
+++ install/init.ps1
@@ -1,10 +1,13 @@
-$path = "C:\Easy Git Clone"
-If(!(test-path $path))
-{
-New-Item -ItemType Directory -Force -Path $path
-}
+$path = "C:\EasyGitClone"
 
+#Creates Directory if it doenst exist
+If(!(test-path $path)){
+New-Item -ItemType Directory -Force -Path $path}
+
+#Copies files during install
 $src = ".\files"
 $dst = $path
 
 Get-ChildItem $src | Copy-Item -Destination $dst -Force
+
+#Writing to registry
